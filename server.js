c3a7a28f1494fbc2c829a85b6b5b3ed4c4e0c61f
rohansinha01 <rohansinha01@gmail.com>
2023-12-12 23:32:43 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -18,5 +18,11 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
     res.send(`<h1>${tipTotal}</h1>`)
 })
 
+app.get("/magic/:url", (req,res) => {
+    const answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+    const randomIndex = Math.floor(Math.random() * answers.length)
+    const randomAnswer = answers[randomIndex]
+    const question = req.params.url
+    res.send(`<h1>${randomAnswer}</h1>`)
+})
 app.listen(3000, () => {console.log("server is listening on port 3000")})
-
