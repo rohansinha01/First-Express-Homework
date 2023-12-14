e629536d842a2273ab8c6b499491ea60abf2aba1
rohansinha01 <rohansinha01@gmail.com>
2023-12-14 18:41:06 -0500

    finished all the beer

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -1,15 +1,28 @@
 const express = require("express")
 const app = express()
-const beer = 99
-app.get('/', (req,res) => {
-    
-     res.send(`<h1>${beer} Bottles of beer on the wall</h1><a href="localhost:3000/">take one down, pass it around</a></h1>`)
+const startingBeer = 99
+const remainingBeer = []
+for(let a = 0; a < 99; a++ ){
+    remainingBeer.push(a)
+}
+console.log(remainingBeer)
+
+
+app.get('/', (req,res) => {  
+     res.send(`<h1>99 Bottles of beer on the wall</h1><h1><a href="${startingBeer - 1}">Take one down, pass it around</a></h1>`)
+    })
+   
+app.get('/:number_of_bottles', (req,res) => { 
+    const number_of_bottles = req.params.number_of_bottles
+    const beer = remainingBeer[number_of_bottles]
+    if (beer > 0){
+    res.send(`<h1>${beer} Bottles of beer on the wall</h1><h1><a href="${beer - 1}">Take one down, pass it around</a></h1>`)
+    } else {
+        res.send(`<h1><a href="${'/'}">Buy More Beer</a></h1>`)
+    }
 })
 
-app.get('/:remainingBeer', (req, res) => {
-    const remainingBeer = req.method.remainingbeer - 1
-    const remaining = beer - 1
-    res.send(`<h1>${remaining} Bottles of beer on the wall</h1><h1><a href="localhost:3000/${remaining}">take one down, pass it around</a></h1>`)
-})
+
+
 
 app.listen(3000, () => {console.log("server is listening on port 3000")})
